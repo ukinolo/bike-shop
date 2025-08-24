c32a13d315a2bd69be51ac6da5002030843c5d41
ukinolo <ykithethird@gmail.com>
2025-08-24 22:29:41 +0200

    add office db

---
+++ BikeShop/OfficeShop/Migrations/20250824202437_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfficeShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Rented = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CustomerId = table.Column<string>(type: "text", nullable: false),
+                    Returned = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bikes");
+        }
+    }
+}
--- BikeShop/OfficeShop/Program.cs
+++ BikeShop/OfficeShop/Program.cs
@@ -1,17 +1,39 @@
-using Domain;
 using Domain.Model;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string[] cities = ["NoviSad", "Kragujevac", "Subotica"];
+
+string? city = null;
+
+if (args.Length >= 1 && cities.Contains(args[0]))
+{
+    city = args[0];
+}
+else
+{
+    city = cities[0];
+}
+
+var connectionString = builder.Configuration.GetConnectionString(city);
+
 // Add services to the container.
-builder.Services.AddDbContext<BikeDbContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<BikeDbContext>(options => options.UseNpgsql(connectionString, dbContextBuilder => dbContextBuilder.MigrationsAssembly("OfficeShop")));
 
 //TODO inject the http client to send requests to check for CentralShop
 
 var app = builder.Build();
+
+{
+    using IServiceScope scope = app.Services.CreateScope();
+
+    using BikeDbContext dbContext =
+        scope.ServiceProvider.GetRequiredService<BikeDbContext>();
+    
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 app.MapGet("/bike", (BikeDbContext db) => db.Bikes);
@@ -20,7 +42,7 @@ app.MapPost("/bike", async (BikeCreateDto bikeCreateDto, BikeDbContext db) =>
 {
     //TODO
     //Add check for Customer existence
-    //If NotFound, send create and rent, and then same
+    //If NotFound, return Bad Request
     //If Bad Request, return Bad Request
     //If Ok, just save new Bike
     
@@ -33,7 +55,7 @@ app.MapPost("/bike", async (BikeCreateDto bikeCreateDto, BikeDbContext db) =>
     await db.SaveChangesAsync();
 });
 
-app.MapPut("/bike/{id}", async (string id, BikeDbContext db) =>
+app.MapPut("/bike/{id}/return", async (string id, BikeDbContext db) =>
 {
     var bike = await db.Bikes.Where(bike => bike.Id == id).FirstOrDefaultAsync();
     if (bike == null)
@@ -52,6 +74,8 @@ app.MapPut("/bike/{id}", async (string id, BikeDbContext db) =>
     await db.SaveChangesAsync();
     return Results.Ok();
 });
+
+app.MapGet("/ping", () => Results.Ok(connectionString));
 
 app.Run();
 
