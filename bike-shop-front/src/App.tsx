751803153a8b7b25513014032aad6d898266267f
ukinolo <ykithethird@gmail.com>
2025-08-28 19:35:51 +0200

    cleanup

--- bike-shop-front/src/App.tsx
+++ bike-shop-front/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import CustomerManagement from './components/CustomerManagement'
 import BikeManagement from './components/BikeManagement'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div id='container'>
